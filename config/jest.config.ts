73a95a24a6f5d46083a5e4e55eb0b270e0fe65f5
ChiElephant <anthony.merino@me.com>
2024-06-28 13:08:10 -0600

    removed babel from jest config

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -1,6 +1,69 @@
-import type { Config } from 'jest'
+// import type { Config } from '@jest/types'
+// import nextJest from '../web/node_modules/next/jest.js'
 
-const config: Config = {
+// const createJestConfig = nextJest({
+//   dir: './web'
+// })
+
+// const config: Config.InitialOptions = {
+//   automock: false,
+//   bail: 2,
+//   clearMocks: false,
+//   collectCoverage: true,
+//   coverageProvider: 'v8',
+//   coverageDirectory: '../coverage',
+//   coverageThreshold: {
+//     global: { branches: 80, functions: 80, lines: 80, statements: -10 }
+//   },
+//   extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+//   notify: true,
+//   notifyMode: 'always',
+//   testMatch: [
+//     '<rootDir>/**/__tests__/**/*.[jt]s?(x)',
+//     '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)'
+//   ],
+//   projects: [
+//     {
+//       displayName: {
+//         name: 'api',
+//         color: 'blue'
+//       },
+//       rootDir: './api',
+//       testEnvironment: 'node',
+//       preset: 'ts-jest',
+//       testPathIgnorePatterns: ['/node_modules/', '/dist/']
+//     },
+//     {
+//       displayName: {
+//         name: 'web',
+//         color: 'magenta'
+//       },
+//       rootDir: './web',
+//       testEnvironment: 'jsdom',
+//       transform: { '^.+\\.(ts|tsx|js)$': 'babel-jest' },
+//       setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+//       moduleNameMapper: {
+//         '^@/(.*)$': '<rootDir>/src/$1'
+//       },
+//       testPathIgnorePatterns: ['/node_modules/', '/.next/', '/public/']
+//     }
+//   ],
+//   resetMocks: false,
+//   resetModules: false,
+//   restoreMocks: false,
+//   verbose: true
+// }
+
+// export default createJestConfig(config)
+
+import type { Config } from '@jest/types'
+import nextJest from '../web/node_modules/next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './web'
+})
+
+const config: Config.InitialOptions = {
   automock: false,
   bail: 2,
   clearMocks: false,
@@ -10,13 +73,16 @@ const config: Config = {
   coverageThreshold: {
     global: { branches: 80, functions: 80, lines: 80, statements: -10 }
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
   notify: true,
   notifyMode: 'always',
-  testMatch: [
-    '<rootDir>/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)'
-  ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['ts-jest', { useEsm: true }]
+  },
+  resetMocks: false,
+  resetModules: false,
+  restoreMocks: false,
+  verbose: true,
   projects: [
     {
       displayName: {
@@ -26,7 +92,10 @@ const config: Config = {
       rootDir: './api',
       testEnvironment: 'node',
       preset: 'ts-jest',
-      testPathIgnorePatterns: ['/node_modules/', '/dist/']
+      testMatch: [
+        '<rootDir>/**/__tests__/**/*.[jt]s?(x)',
+        '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)'
+      ]
     },
     {
       displayName: {
@@ -34,19 +103,18 @@ const config: Config = {
         color: 'magenta'
       },
       rootDir: './web',
-      testEnvironment: 'jsdom',
-      transform: { '^.+\\.(ts|tsx|js)$': 'babel-jest' },
+      testEnvironment: 'jest-environment-jsdom',
       setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
       moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
+        '^@/(.*)$': '<rootDir>/web/src/$1'
       },
+      testMatch: [
+        '<rootDir>/**/__tests__/**/*.[jt]s?(x)',
+        '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)'
+      ],
       testPathIgnorePatterns: ['/node_modules/', '/.next/', '/public/']
     }
-  ],
-  resetMocks: false,
-  resetModules: false,
-  restoreMocks: false,
-  verbose: true
+  ]
 }
 
-export default config
+export default createJestConfig(config)
